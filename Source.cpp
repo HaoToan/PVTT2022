380d9a3dce86451c9953e5fe3f3021d124554f99
vinhnguyen2k1 <thanhvinh951@gmail.com>
2022-10-28 13:50:35 +0700

    Vinh add function nhan(a,b)

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 int cong(int a, int b);//SV A
 int tru(int a, int b);//SV B
-int nhan(int a, int b);//SV C
+int nhan(int a, int b){
+	return a * b;
+}
 int chia(int a, int b);//SV D
 int main()
 {
