3ec40579a7150100e1617f70717cc539efc4597f
juzly2001 <tantaid19@gmail.com>
2022-10-28 13:54:32 +0700

    Tai chia(a.b)

--- Source.cpp
+++ Source.cpp
@@ -4,14 +4,17 @@ using namespace std;
 int cong(int a, int b);//SV A
 int tru(int a, int b);//SV B
 int nhan(int a, int b);//SV C
-int chia(int a, int b);//SV D
+
+int chia(int a, int b){
+	return a / b;
+}
 int main()
 {
 	int a, b;
 	cout << "CHUONG TRINH TINH TOAN CO BAN" << endl;
 	//Cac ban code tai day
-
-	cout << endl;
+	int result = chia(a,b);
+	cout << result <<endl;
 	system("pause");
 	return 0;
 }
