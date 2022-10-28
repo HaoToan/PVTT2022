2106dc663339547ba8f35d5a5275cd05785eb937
HaoToan <nguyenht2208@gmail.com>
2022-10-28 13:49:40 +0700

    Toan add function cong(a,b)

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 using namespace std;
-int cong(int a, int b);//SV A
+int cong(int a, int b){
+return a+b;
+};//SV A
 int tru(int a, int b);//SV B
 int nhan(int a, int b);//SV C
 int chia(int a, int b);//SV D
@@ -10,7 +12,7 @@ int main()
 	int a, b;
 	cout << "CHUONG TRINH TINH TOAN CO BAN" << endl;
 	//Cac ban code tai day
-
+	cout<<"Tong 2 so 3 va 5 la : " <<tong(3,5);
 	cout << endl;
 	system("pause");
 	return 0;
